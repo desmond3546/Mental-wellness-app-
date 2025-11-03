ac3d83494a915a52b4c92ed0ae631fecbc454ff2
desmond3546 <aaravgaur759@gmail.com>
2025-11-03 16:52:13 +0530

    app.py

--- app.py
+++ app.py
@@ -474,4 +474,8 @@ def chat():
 # Run App
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets this automatically
+    app.run(host="0.0.0.0", port=port)
+
+
